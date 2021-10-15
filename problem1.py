dd95294b256690e6a1fa8ce66cf8119ca762e429
dhhruv <180320107529.ce.dhruv@gmail.com>
2021-10-15 14:10:55 +0530

    Reformatted Previous Python Codes and Added My New Python Codes

--- problem1.py
+++ problem1.py
@@ -1,11 +1,12 @@
 # code here
 import numpy as np
+
 n_arr = np.array([75.523, 42.0, 65.32924763])
 print("Given array:")
 print(n_arr)
-  
+
 print("\nReplace all h are greater than 50. to 15.50")
-n_arr[n_arr > 50.] = 15.50
-  
+n_arr[n_arr > 50.0] = 15.50
+
 print("New array :\n")
 print(n_arr)
--- problem2.py
+++ problem2.py
@@ -1,13 +1,12 @@
 # code here
 
 
-
 import pandas as pd
 
-d1 =[[2, 3, 4], [5, 6, 7]]
+d1 = [[2, 3, 4], [5, 6, 7]]
 
-d2 =[[2, 4, 8], [1, 3, 9]]
+d2 = [[2, 4, 8], [1, 3, 9]]
 
-Data ={'first': d1, 'second': d2}
+Data = {"first": d1, "second": d2}
 
-df2d = pd.DataFrame(Data)	
+df2d = pd.DataFrame(Data)
